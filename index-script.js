5c1ec730ee6a8bb8114c0dcbe9488fcff4a2df0d
derynm <dery02.nm@gmail.com>
2025-03-01 15:50:27 +0700

    feat: display data employee

--- index-script.js
+++ index-script.js
@@ -8,5 +8,85 @@
    * - Add a function to filter data by employee's name
    * - Add a function to display table
    * - Add a function to delete data (bonus)
-   */
+   */ 
+
+  let employees = [];
+
+  function redirectToForm() {
+    window.location.href = 'form.html';
+  }
+
+  const renderTableEmployees = () => {
+    const table = document.getElementById('employee-table-body');
+    table.innerHTML = '';
+
+    employees.forEach((employee) => {
+      table.innerHTML += `
+        <tr>
+          <td>${employee.id}</td>
+          <td>${employee.name}</td>
+          <td>${employee.position}</td>
+          <td>
+            <button class="btn-secondary btn-delete" id="btn-${employee.id}">Delete</button>
+          </td>
+        </tr>
+      `;
+      
+    });
+  }
+
+  const renderLoading = () => {
+    const table = document.getElementById('employee-table-body');
+    table.innerHTML = `
+      <tr>
+        <td colspan="4">Loading...</td>
+      </tr>
+    `;
+  }
+
+  const getEmployees = async () => {
+    renderLoading();
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    employees = data;
+    renderTableEmployees();
+  }
+
+  const searchEmployee = async () => {
+    renderLoading();
+    const searchInput = document.getElementById('search-input').value;
+    const response = await fetch(`${API_URL}?name=${searchInput}`);
+    const data = await response.json();
+    employees = data;
+    renderTableEmployees();
+  }
+
+  const deleteEmployee = async (id) => {
+    const response = await fetch(`${API_URL}/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      getEmployees();
+    }
+  }
+
+  
+
+  document.addEventListener('DOMContentLoaded', () => {
+    getEmployees();
+  });
+  document.getElementById('button-to-form').addEventListener('click', redirectToForm);
+  document.getElementById('button-search-employee').addEventListener('click', (event) => { 
+    event.preventDefault()
+    searchEmployee()
+  });
+
+  document.getElementById('employee-table-body').addEventListener('click', (event) => {
+    if (event.target.classList.contains('btn-delete')) {
+      const id = event.target.id.split('-')[1];
+      deleteEmployee(id);
+    }
+  });
+
 })();
